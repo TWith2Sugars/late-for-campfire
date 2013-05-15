1379affd6c89faa4db03d81c7de678957eb5feef
Tony Williams <tony.williams@thap.co.uk>
2013-05-15 20:28:35 +0100

    Also checks direct messages which Fixes #1

--- LateForCampfire/Twitter.fs
+++ LateForCampfire/Twitter.fs
@@ -4,7 +4,7 @@ open System
 open System.IO
 open TweetSharp
 
-type Tweet = { Text:string; User:string }
+type Tweet = { Text:string; User:string; Id:int64; UserId:int64 }
 
 let private path = "last-tweet-id.txt"
 
@@ -17,18 +17,27 @@ let private fetchLastTweetId =
         if success then Some(id)
         else None
 
-let private setLastTweetId (tweet:TwitterStatus) =
+let private setLastTweetId (tweet:Tweet) =
     File.WriteAllText(path, tweet.Id.ToString())
 
-let private getMentions (twitterService : TwitterService) = 
-    let options = ListTweetsMentioningMeOptions()
+let private getMessages (twitterService : TwitterService) = 
+    let mentionOptions = ListTweetsMentioningMeOptions()
+    let directOptions = ListDirectMessagesReceivedOptions()
     match fetchLastTweetId with
-    | Some id -> options.SinceId <- Nullable<int64> id
+    | Some id -> mentionOptions.SinceId <-  Nullable<int64> id; directOptions.SinceId <-  Nullable<int64> id
     | None -> ()    
 
-    let mentions = twitterService.ListTweetsMentioningMe options
-    if not (Seq.isEmpty mentions) then
-        mentions 
+    let mentions = 
+        twitterService.ListTweetsMentioningMe mentionOptions
+        |>  Seq.map (fun x -> {Text=x.Text; User=x.User.Name; Id=x.Id; UserId=x.User.Id}) 
+
+    let direct = 
+        twitterService.ListDirectMessagesReceived directOptions
+        |>  Seq.map (fun x -> {Text=x.Text; User=x.Sender.Name; Id=x.Id; UserId=x.Sender.Id}) 
+    
+    let messages = [ mentions; direct] |> Seq.concat
+    if not (Seq.isEmpty messages) then
+        messages
         |> Seq.maxBy (fun x -> x.Id) 
         |> setLastTweetId
 
@@ -43,13 +52,12 @@ let private getRestrictedUsers (twitterService : TwitterService) user listName =
     membershipOptions.ListId <- System.Nullable<int64> restrictedList.Id
     twitterService.ListListMembers membershipOptions
 
-let private matchTweet (tweet:TwitterStatus) (list:TwitterCursorList<TwitterUser>) =
-    list |> Seq.exists (fun u -> u.Id = tweet.User.Id)
+let private matchTweet (tweet:Tweet) (list:TwitterCursorList<TwitterUser>) =
+    list |> Seq.exists (fun u -> u.Id = tweet.UserId)
 
 let getTweets consumerKey consumerSecret accessToken accessSecret user listName = 
     let twitterService = TwitterService(consumerKey, consumerSecret, accessToken, accessSecret)
-    let tweets = getMentions twitterService
+    let tweets = getMessages twitterService
     let userList = getRestrictedUsers twitterService user listName
     tweets 
     |> Seq.filter (fun x -> matchTweet x userList)
-    |> Seq.map (fun x -> {Text=x.Text; User=x.User.Name})
