59264ecf1499b394875873c1d6d64890655f5fe8
Tony Williams <tony.williams@thap.co.uk>
2013-05-16 11:05:01 +0100

    Fixed a few problems

--- LateForCampfire/Twitter.fs
+++ LateForCampfire/Twitter.fs
@@ -6,50 +6,82 @@ open TweetSharp
 
 type Tweet = { Text:string; User:string; Id:int64; UserId:int64 }
 
-let private path = "last-tweet-id.txt"
+let private lastMention = "last-tweet-id.txt"
+let private lastDirectMessage = "last-direct-tweet-id.txt"
 
-let private fetchLastTweetId = 
+let private fetchLastTweetId path = 
     if not (File.Exists path) then 
-        None
+        Nullable<int64>()
     else
         let text = File.ReadAllText path
         let success, id = Int64.TryParse text
-        if success then Some(id)
-        else None
+        if success then Nullable<int64>(id)
+        else Nullable<int64>()
 
-let private setLastTweetId (tweet:Tweet) =
-    File.WriteAllText(path, tweet.Id.ToString())
+let private setLastTweetId (path, tweetId:int64) =
+    File.WriteAllText(path, tweetId.ToString())
+
+let private setLastDirectMessageId (tweet:TwitterDirectMessage) = 
+    setLastTweetId(lastDirectMessage, tweet.Id)
+
+let private setLastMentionMessageId (tweet:TwitterStatus) = 
+    setLastTweetId(lastMention, tweet.Id)
 
 let private getMessages (twitterService : TwitterService) = 
     let mentionOptions = ListTweetsMentioningMeOptions()
+    mentionOptions.SinceId <- fetchLastTweetId lastMention
+
     let directOptions = ListDirectMessagesReceivedOptions()
-    match fetchLastTweetId with
-    | Some id -> mentionOptions.SinceId <-  Nullable<int64> id; directOptions.SinceId <-  Nullable<int64> id
-    | None -> ()    
+    directOptions.SinceId <- fetchLastTweetId lastDirectMessage
+       
+    let rawMentions = 
+        match twitterService.ListTweetsMentioningMe mentionOptions with
+        | null -> Seq.empty<TwitterStatus>
+        | x -> x
 
     let mentions = 
-        twitterService.ListTweetsMentioningMe mentionOptions
-        |>  Seq.map (fun x -> {Text=x.Text; User=x.User.Name; Id=x.Id; UserId=x.User.Id}) 
+        if Seq.isEmpty rawMentions then
+            Seq.empty<Tweet>
+        else
+            rawMentions
+            |> Seq.maxBy (fun x -> x.Id) 
+            |> setLastMentionMessageId
+            |> ignore
+
+            rawMentions 
+            |> Seq.map (fun x -> {Text=x.Text; User=x.User.Name; Id=x.Id; UserId=x.User.Id})
+
+    let rawDirect = 
+        match  twitterService.ListDirectMessagesReceived directOptions  with
+        | null -> Seq.empty<TwitterDirectMessage>
+        | x -> x
 
     let direct = 
-        twitterService.ListDirectMessagesReceived directOptions
-        |>  Seq.map (fun x -> {Text=x.Text; User=x.Sender.Name; Id=x.Id; UserId=x.Sender.Id}) 
-    
-    let messages = [ mentions; direct] |> Seq.concat
-    if not (Seq.isEmpty messages) then
-        messages
-        |> Seq.maxBy (fun x -> x.Id) 
-        |> setLastTweetId
+        if Seq.isEmpty rawDirect then
+            Seq.empty<Tweet>
+        else
+            rawDirect
+            |> Seq.maxBy (fun x -> x.Id) 
+            |> setLastDirectMessageId
+            |> ignore
 
-    messages
+            rawDirect 
+            |> Seq.map (fun x -> {Text=x.Text; User=x.Sender.Name; Id=x.Id; UserId=x.Sender.Id})
+    
+    [ mentions; direct] |> Seq.concat
+   
 
 let private getRestrictedUsers (twitterService : TwitterService) user listName =
     let options = GetListOptions()
     options.OwnerScreenName <- user
     options.Slug <- listName
-    let restrictedList = twitterService.GetList options
+    let restrictedListId = 
+        match twitterService.GetList options with
+        | null ->  System.Nullable<int64>()
+        | x ->  System.Nullable<int64> x.Id
+
     let membershipOptions = ListListMembersOptions()
-    membershipOptions.ListId <- System.Nullable<int64> restrictedList.Id
+    membershipOptions.ListId <- restrictedListId
     twitterService.ListListMembers membershipOptions
 
 let private matchTweet (tweet:Tweet) (list:TwitterCursorList<TwitterUser>) =
